9c55234f8741c1e2988cc8557e41acc94ce9248e
david hernandez <davidfelipehernandez1@gmail.com>
2023-02-17 11:47:46 -0500

    add routes model

---
+++ src/models/routes.models.ts
@@ -0,0 +1,11 @@
+export const publicRoutes = {
+  HOME: '',
+  GLOBAL: 'global',
+  CARGA: 'carga',
+  DISTRIBUCION: 'distribucion',
+  ULTIMAMILLA: 'ultima-milla',
+  SMARTQUICK: 'smart-quick',
+  VINCULATE: 'vinculate',
+  BLOG: 'blog',
+  CONTACTO: 'contacto',
+};
