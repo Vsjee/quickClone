f6f0bf11280dc73041c46cd05a7177bb122e2006
david hernandez <davidfelipehernandez1@gmail.com>
2023-02-16 22:57:23 -0500

    add setup basic routing

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import { createApp } from 'vue';
+import App from './App.vue';
+import './style.css';
 
-createApp(App).mount('#app')
+const routes = [{ path: '/', component: () => import('./pages/Home.vue').then((p) => p.default) }];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
