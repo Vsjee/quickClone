645e89700a66079d7765f243e7dd2de83ff092ff
david hernandez <davidfelipehernandez1@gmail.com>
2023-02-17 11:39:03 -0500

    add data for the service card

---
+++ src/data/ServicesData.data.ts
@@ -0,0 +1,28 @@
+import { IServiceCard } from '../types';
+
+export const ServicesData: IServiceCard[] = [
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/global.svg',
+    title: 'GLOBAL',
+    subTitle: 'Transporte de Carga Internacional',
+    info: 'Movilizamos tus mercancías desde y hacia cualquier lugar del mundo.',
+  },
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/carga.svg',
+    title: 'CARGA',
+    subTitle: 'Transporte de Carga Nacional',
+    info: 'Transportamos tu carga desde puertos o entre ciudades con vehículos pesados.',
+  },
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/carga.svg',
+    title: 'DISTRIBUCIÓN',
+    subTitle: 'Logística de Primera Milla',
+    info: 'Distribuimos tus productos desde centros de distribución hasta canales de ventas.',
+  },
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/milla.svg',
+    title: 'ÚLTIMA MILLA',
+    subTitle: 'Entregas de Última Milla',
+    info: 'Entregamos tus envíos, ventas e-commerce o documentos a tus consumidores.',
+  },
+];
