fc144df1789a797dcd1f396c82e72a36bf48a655
david hernandez <davidfelipehernandez1@gmail.com>
2023-02-18 19:11:38 -0500

    add data fot the slider

---
+++ src/data/sliderData.data.ts
@@ -0,0 +1,34 @@
+import { SliderInfoI } from '../types';
+
+export const sliderData: SliderInfoI[] = [
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/route1.webp',
+    title: 'Ruteo',
+    text: 'Tendrás la oportunidad de planificar tus entregas, generando rutas óptimas de acuerdo a las variables que definas en el proceso logístico a realizar.',
+  },
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/cubing1.webp',
+    title: 'Cubicaje',
+    text: 'Aprovecharás la capacidad máxima del vehículo, con un sistema diseñado por nosotros mismos que optimiza tus espacios y disminuye tus costos de transporte.',
+  },
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/control1.webp',
+    title: 'Control de entregas',
+    text: 'Gestionarás con éxito tus entregas, ya que tendrás en tiempo real KPI´s e información de novedades con evidencias fotográficas y digitales.',
+  },
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/kpi1.webp',
+    title: 'KPIs',
+    text: 'Recibirás indicadores por estado (entrega, alistamiento, llegada), sabrás el número de tareas por sede o recurso y el rango de hora por actividad a realizar.',
+  },
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/geo1.webp',
+    title: 'Georreferenciación',
+    text: 'Asignarás coordenadas geográficas que identificarán las direcciones ingresadas al sistema, para que posteriormente la información sea señalizada en el mapa.',
+  },
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/zone1.webp',
+    title: 'Zonificación',
+    text: 'Lograrás dividir y organizar racionalmente una ciudad, área o territorio, creando zonas homogéneas de acuerdo a todas tus necesidades logísticas.',
+  },
+];
