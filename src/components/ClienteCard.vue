dc310b7b4b690ce974b597f892cffe9be10ee8ee
david hernandez <davidfelipehernandez1@gmail.com>
2023-02-24 23:06:13 -0500

    add basic clientCard comp

---
+++ src/components/ClienteCard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ISomeClientsData } from '../types';
+
+defineProps<{ clientsData: ISomeClientsData }>();
+</script>
+
+<template>
+  <picture>
+    <img :src="clientsData.img" :alt="clientsData.name" :title="clientsData.name" />
+  </picture>
+</template>
+
+<style scoped></style>
