d92fb16ba306b037811c6f197f0418020a71315b
david hernandez <davidfelipehernandez1@gmail.com>
2023-02-24 22:54:36 -0500

    add client data and use types

---
+++ src/data/someClientsData.data.ts
@@ -0,0 +1,36 @@
+import { ISomeClientsData } from '../types';
+
+export const someClientsData: ISomeClientsData[] = [
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/exito1.webp',
+    name: 'Exito',
+  },
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/loreal1.webp',
+    name: 'Loreal',
+  },
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/nestle1.webp',
+    name: 'Nestle',
+  },
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/johnson1.webp',
+    name: 'Johnson',
+  },
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/abinbev1.webp',
+    name: 'Abinbev',
+  },
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/nutresa1.webp',
+    name: 'Nutresa',
+  },
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/samsung1.webp',
+    name: 'samsung',
+  },
+  {
+    img: 'https://quick.com.co/wp-content/uploads/2022/10/unilever1.webp',
+    name: 'Unilever',
+  },
+];
